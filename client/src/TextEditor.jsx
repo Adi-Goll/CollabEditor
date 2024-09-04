68f0bf101789d10b9751dfd9aa859d306010cb0f
Adi-Goll <adigollamudi@gmail.com>
2024-09-04 17:02:59 -0400

    added server and client folders + basic server setup

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -6,6 +6,20 @@ import 'quill/dist/quill.snow.css'
 export default function TextEditor() {
 
 
+    const toolbarOptions = [['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+    ['link', 'image', 'video'],
+
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
+    [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+
+    ];
 
     // useCallback lets us store a function definition between renders and makes sure
     // that the function is not continuously called everytime the page re-renders
@@ -16,11 +30,20 @@ export default function TextEditor() {
         const editor = document.createElement("div")
         wrapper.append(editor)
 
-        const quill = new Quill(editor, { theme: 'snow' })
-
+        const quill = new Quill(editor, {
+            theme: 'snow',
+            modules: {
+                toolbar: toolbarOptions
+            }
+        })
 
 
     }, [])
+
+    const insertDropDown = () => {
+        console.log("testing")
+    }
+
 
 
     // editorContainer will store the function definition returned by the useCallback and then on subsequent 
